38f9d0ee9545ac5a6741c2c4310f92b5fa00100d
honor <dobrokn@gmail.com>
2025-03-21 18:43:06 +0300

    fix index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import firebase_admin
+from firebase_admin import credentials, db
 
 app = Flask(__name__)
 
+# Инициализация Firebase
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://mypolka-4cf39-default-rtdb.firebaseio.com/'
+})
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    ref = db.reference('appointments')
+    appointments = ref.get()
+    return render_template('dashboard.html', appointments=appointments)
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
